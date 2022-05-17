abbc57d83ebca686f7bf0835cb5f4a505f6e4ca0
raymondkwok7877 <rkwok7877@gmail.com>
2022-05-17 19:22:40 -0400

    Added extra images and cleaned up more code

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,11 +4,15 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
+import '../../index.css';
+import collegeImage from '../../img/college.png';
+
 const HomePageView = () => {
   // Render Home page view
   return (
     <div >
       <h1>Home Page</h1>
+      <img id = "homeImage" src={collegeImage} alt="college"></img>
     </div>
   );    
 }
